b84745a5e47e1ec73e19383a9cb6b5d7676041fc
Anna <anusia.gottfried@gmail.com>
2020-07-22 11:47:10 +0200

    zakupy1.py

--- zakupy1.py
+++ zakupy1.py
@@ -2,3 +2,13 @@ zakupy={ 'Piekarnia': ['Chleb', 'Pączek', 'Bułki'],
 'Warzywniak':['Marchew', 'Seler', 'Rukola']
 }
 print('Lista zakupow')
+
+ilosc_przedmiotow=0
+for place, item in zakupy.items():
+    print('Ide do {}, kupuje {}'.format(place,item))
+    for produkt in item:
+        ilosc_przedmiotow+=1
+
+
+print('W sumię kupuje {} produktów'.format(ilosc_przedmiotow))        
+
