d40b3a1fad7621c2a384d032e65562be10588211
Anna <anusia.gottfried@gmail.com>
2020-07-29 18:38:43 +0200

    dodanie zwracania wartosci funkcji false lub true

--- palindrom.py
+++ palindrom.py
@@ -4,8 +4,10 @@ def palindrom(tekst):
     print(txt)    
     if tekst==txt:
         print(' nazwa %s jest palindromem'% tekst)
+        return True
     else:
-         print(' nazwa %s nie jest palindromem'% tekst)   
+         print(' nazwa %s nie jest palindromem'% tekst) 
+         return False  
 
 palindrom('kajak')
 
